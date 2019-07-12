436b52a59227c27301a9762f07e57e090d7f2584
Zili Xiang <zx77@cornell.edu>
2019-07-12 00:00:17 -0500

    play with the actix services

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, Responder, HttpServer, HttpResponse};
+use std::cell::Cell;
 
 fn index(info: web::Path<(String, u32)>) -> impl Responder {
     format!("hello, {}! id: {}", info.0, info.1)
@@ -8,8 +9,22 @@ fn root() -> impl Responder {
     HttpResponse::Ok().body("bello!")
 }
 
+struct AppState {
+    visitor_count: Cell<i32>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        AppState {
+            visitor_count: Cell::new(0)
+        }
+    }
+}
+
 fn main() -> std::io::Result<()> {
+    // could use .configure(Fn) as well to set testing routes
     HttpServer::new(|| App::new()
+        .data(AppState::new())
         .service(
         web::resource("/{name}/{id}/index.html").to(index)
         )
